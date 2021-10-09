9f3672afa40c4c6d2c77976d50266b411d1d9019
Telmo Sampaio <telmoasampaio@gmail.com>
2021-10-09 12:15:25 +0100

    adding static folder

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World');
+// });
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
